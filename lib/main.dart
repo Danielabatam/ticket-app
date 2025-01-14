3eff2d804087909181c1af041cc7d4f9b7053654
Daniela BATAM <danielabatam@gmail.com>
2025-01-14 17:46:33 +0100

    Reformating

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
-  var car = Car(4);
-  car.carType();
-  car.wheelsNum();
-  var plane = Plane(3);
-  plane.wheelsNum();
-
-  //runApp(const MyApp());
+  runApp(const MyApp());
 }
-
-
-abstract class Vehicule {
-  Vehicule(this.wheels);
-  int wheels;
-  void wheelsNum();
-}
-
-class Car extends Vehicule {
-  Car(super.wheels);
-
-  void carType() {
-    print("land runner");
-  }
-
-  @override
-  void wheelsNum() {
-    print("The car's wheels number is $wheels");
-  }
-}
-
-class Plane extends Vehicule {
-  Plane(super.wheels);
-
-  void planeType() {
-    print("land runner");
-  }
-
-  @override
-  void wheelsNum() {
-    print("The plane's wheels number is $wheels");
-  }
-}
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-                                                                                                                                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -64,8 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-      
-        
   final String title;
 
   @override
@@ -77,20 +34,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-                                    _counter++;
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-                            return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-                                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                        title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
       body: Center(
-                        child: Column(
-                                                                                                                                            mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
@@ -106,6 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ),     );
+      ),
+    );
   }
 }
